6201ce663f95341db23a7d40e2fd1d21319f0333
ZoeyF75 <zf.zoodle@gmail.com>
2021-06-01 17:41:47 -0700

    Core data types in C# same as C++

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,15 +10,12 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            string characterName = "John";
-            int characterAge;
-            characterAge = 35;
-            Console.WriteLine("There once was a man named " + characterName);
-            Console.WriteLine("He was " + characterAge +  " years old");
-            Console.WriteLine("He really like the name " + characterName);
-            Console.WriteLine("But he didn't like being " + characterAge);
+            string phrase = "Giraffe Academy";
+            char grade = 'A';
+            int age = 30;
+            double gpa = 3.3;
+            bool isMale = true;
 
-            Console.ReadLine();
         }
     }
 }
