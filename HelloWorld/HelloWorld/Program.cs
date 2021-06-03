2302b8e6777419bb2aac0e3e67369ce88a6e797b
ZoeyF75 <zf.zoodle@gmail.com>
2021-06-02 21:04:19 -0700

    String methods and functionality

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,7 +10,12 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Giraffe\"Academy");
+            string phrase = "Giraffe Academy";
+            Console.WriteLine(phrase.ToUpper());
+            Console.WriteLine(phrase.Contains("Academy"));
+            Console.WriteLine(phrase[0]); //first letter
+            Console.WriteLine(phrase.IndexOf("Academy")); //8 starts at index 8, -1 if string doesn't contain
+            Console.WriteLine(phrase.Substring(8)); //prints out Academy
 
             Console.ReadLine();
 
