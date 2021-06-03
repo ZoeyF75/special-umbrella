421f91a991620c6ddd0f38e98ed4dbc8c9cd0c35
ZoeyF75 <zf.zoodle@gmail.com>
2021-06-02 20:52:01 -0700

    newline character

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,11 +10,9 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            string phrase = "Giraffe Academy";
-            char grade = 'A';
-            int age = 30;
-            double gpa = 3.3;
-            bool isMale = true;
+            Console.WriteLine("Giraffe\nAcademy");
+
+            Console.ReadLine();
 
         }
     }
