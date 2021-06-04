0085f4dbc090ec802ff67a4741b848d914b00252
ZoeyF75 <zf.zoodle@gmail.com>
2021-06-03 23:10:33 -0700

    Solution

--- 2D-Arrays-HackerRank/2D-Arrays-HackerRank/Program.cs
+++ 2D-Arrays-HackerRank/2D-Arrays-HackerRank/Program.cs
@@ -1,12 +1,67 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-namespace DArraysHackerRank
+class Result
 {
-    class MainClass
+
+    /*
+     * Complete the 'hourglassSum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+    //arr[0,0] [0, +1], [0, +1] top
+    //arr[+1, +1] middle
+    //arr[+1, 0], [stay, +1] [stay, +1] bottom
+
+    public static int hourglassSum(List<List<int>> arr)
     {
-        public static void Main(string[] args)
+        List<int> sums = new List<int>();
+
+        for (int y = 1; y < 5; y++)
         {
-            Console.WriteLine("Hello World!");
+            for (int x = 1; x < 5; x++)
+            {
+                int sum = arr[y - 1][x - 1] + arr[y - 1][x] + arr[y - 1][x + 1]
+                                                   + arr[y][x] +
+                          arr[y + 1][x - 1] + arr[y + 1][x] + arr[y + 1][x + 1];
+                sums.Add(sum);
+            }
         }
+        return sums.Max();
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        List<List<int>> arr = new List<List<int>>();
+
+        for (int i = 0; i < 6; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+        }
+
+        int result = Result.hourglassSum(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
     }
 }
