1266a23b47e35296799f722c95bcfd5f3d0d52c6
ZoeyF75 <zf.zoodle@gmail.com>
2021-06-03 22:55:24 -0700

    hacker rank algorithim practice

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,24 +10,7 @@ namespace Giraffe
     {
         static void Main(string[] args) //main Method
         {
-            try
-            {
-                Console.Write("Enter a number: ");
-                int num1 = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Enter another number: ");
-                int num2 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine(num1 / num2);
-
-            }
-            catch(DivideByZeroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            catch(FormatException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            
+           
             Console.ReadLine();
         }
 
