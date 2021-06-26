e034afa94fc0ef8eb37c2518b245504492983f64
ZoeyF75 <zf.zoodle@gmail.com>
2021-06-26 16:43:47 -0700

    test dome practice alert service class solution

---
+++ TestDome/TestDome/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestDome
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Round(6.5));
+            Console.WriteLine(Math.Round(11.5));
+        }
+    }
+}
